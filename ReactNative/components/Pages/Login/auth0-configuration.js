8d1a41130adac317be03b4c9359e709aece0caf6
Nathaniel Anderson <nathanielmotion@gmail.com>
2020-03-03 19:23:40 -0700

    added login page component

---
+++ ReactNative/components/Pages/Login/auth0-configuration.js
@@ -0,0 +1,5 @@
+import {CLIENT_ID,AUTH0_DOMAIN} from "react-native-dotenv";
+module.exports = {
+  clientId: CLIENT_ID,
+  domain: AUTH0_DOMAIN,
+};
--- ReactNative/components/Test/index.js
+++ ReactNative/components/Test/index.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {View, Button} from 'react-native';
-import { PRIMARY_DOMAIN } from 'react-native-dotenv';
+import {PRIMARY_DOMAIN} from 'react-native-dotenv';
 
-export default class Test extends Component{
-    state = {
-        text: "Hello"
-    }
+export default class Test extends Component {
+  state = {
+    text: 'Hello',
+  };
 
-    testPress = () => {
-        fetch(PRIMARY_DOMAIN, {
-            method: `GET`
-        })
-        .then(response => response.json())
-        .then(responseJson => {
-            this.setState({
-                text: responseJson.body
-            })
-        .catch(err => {
-            console.error(err);
-        })
+  testPress = () => {
+    fetch(PRIMARY_DOMAIN, {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          text: responseJson.body,
+        }).catch(err => {
+          console.error(err);
         });
-    }
+      });
+  };
 
-    render(){
-        return(
-            <View>
-                <Button onPress={this.testPress} title={this.state.text}/>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <Button onPress={this.testPress} title={this.state.text} />
+      </View>
+    );
+  }
 }
