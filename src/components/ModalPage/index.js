835a97a839a703fa12741921d3fd87b39e804c4d
Brittany Huber <brittanyleighhuber@gmail.com>
2020-03-08 17:18:22 -0600

    Added modals to the different event buttons

--- src/components/ModalPage/index.js
+++ src/components/ModalPage/index.js
@@ -1,32 +1,59 @@
 import React, { Component } from 'react';
 import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
+import './style.css';
+import StartLocation from "../../views/StartLocation";
+import StartLocationButton from '../StartLocationButton';
 
 class ModalPage extends Component {
+  
 state = {
-  modal: false
+  modal1: false,
+  modal2: false,
+  modal3: false,
+  modal4: false
 }
 
-toggle = () => {
+toggle = nr => () => {
+  let modalNumber = 'modal' + nr
   this.setState({
-    modal: !this.state.modal
+    [modalNumber]: !this.state[modalNumber]
   });
 }
 
 render() {
   return (
-    <MDBContainer>
-      <MDBBtn onClick={this.toggle}>Modal</MDBBtn>
-      <MDBModal isOpen={this.state.modal} toggle={this.toggle}>
-        <MDBModalHeader toggle={this.toggle}>MDBModal title</MDBModalHeader>
-        <MDBModalBody>
-          (...)
-        </MDBModalBody>
-        <MDBModalFooter>
-          <MDBBtn color="secondary" onClick={this.toggle}>Close</MDBBtn>
-          <MDBBtn color="primary">Save changes</MDBBtn>
-        </MDBModalFooter>
-      </MDBModal>
-    </MDBContainer>
+      <MDBContainer>
+        <StartLocationButton text="Stop Trip"></StartLocationButton>
+        <MDBBtn className="modal-btn" color="primary" onClick={this.toggle(1)}>Excuse Call</MDBBtn>
+        <MDBModal isOpen={this.state.modal1} toggle={this.toggle(14)} centered>
+          <MDBModalHeader toggle={this.toggle(1)}>Message Sent</MDBModalHeader>
+          <MDBModalFooter>
+            <MDBBtn className="dismiss-modal" onClick={this.toggle(1)}>Dismiss</MDBBtn>
+          </MDBModalFooter>
+        </MDBModal>
+        <MDBBtn className="modal-btn" color="primary" onClick={this.toggle(2)}>Request Pickup</MDBBtn>
+        <MDBModal isOpen={this.state.modal2} toggle={this.toggle(2)} centered>
+          <MDBModalHeader className="modal-header" toggle={this.toggle(2)}>Message sent</MDBModalHeader>
+          <MDBModalFooter>
+          <MDBBtn className="dismiss-modal" onClick={this.toggle(1)}>Dismiss</MDBBtn>
+          </MDBModalFooter>
+        </MDBModal>
+        <MDBBtn className="modal-btn" color="primary" onClick={this.toggle(3)}>Request Emergency Assistance</MDBBtn>
+        <MDBModal isOpen={this.state.modal3} toggle={this.toggle(3)} centered>
+          <MDBModalHeader toggle={this.toggle(3)}>Message Sent</MDBModalHeader>
+          <MDBModalFooter>
+          <MDBBtn className="dismiss-modal" onClick={this.toggle(1)}>Dismiss</MDBBtn>
+          </MDBModalFooter>
+        </MDBModal>
+        <MDBBtn className="modal-btn" color="primary" onClick={this.toggle(4)}>Update Location</MDBBtn>
+        <MDBModal isOpen={this.state.modal4} toggle={this.toggle(4)} centered>
+          <MDBModalHeader toggle={this.toggle(4)}>Location Updated</MDBModalHeader>
+          <MDBModalFooter>
+          <MDBBtn className="dismiss-modal" onClick={this.toggle(1)}>Dismiss</MDBBtn>
+          </MDBModalFooter>
+        </MDBModal>
+      </MDBContainer>
+      
     );
   }
 }
