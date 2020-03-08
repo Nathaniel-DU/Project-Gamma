b2eb57b8efa63cf993d07deb9e31b51fad7bd667
Brittany Huber <brittanyleighhuber@gmail.com>
2020-03-08 17:18:22 -0600

    Added modals to the different event buttons

--- src/views/UpdateView/index.js
+++ src/views/UpdateView/index.js
@@ -1,19 +1,15 @@
 import React, { Fragment } from "react";
-import StartLocationButton from "../../components/StartLocationButton"
+import ModalPage from "../../components/ModalPage";
 import './style.css';
+
 
 const StartLocation = () => (
   
   <Fragment>
     <h1>StaySafe</h1>
     <div className="button-list">
-      <StartLocationButton className="stop-btn" text='Stop'></StartLocationButton>
-      <StartLocationButton className="excuse-btn" text='Excuse Call'></StartLocationButton>
-      <StartLocationButton className="pickup-btn" text='Ask for Pickup'></StartLocationButton>
-      <StartLocationButton className="panic-btn" text='Request Emergency Assistance'></StartLocationButton>
-      <StartLocationButton className="update-loc-btn" text='Update My Location'></StartLocationButton>
+      <ModalPage/>
     </div>
-    
   </Fragment>
 );
 
