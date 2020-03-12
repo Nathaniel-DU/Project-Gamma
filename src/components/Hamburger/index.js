4981b942747719f80ef2029a2ca9e09f19922124
Bradley Knutson <bradley@bknutson.com>
2020-03-12 12:55:45 -0600

    Added signout button componenent and added to hamburger icon to be ready for styling

--- src/components/Hamburger/index.js
+++ src/components/Hamburger/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { stack as Menu } from 'react-burger-menu'
+import Signout from '../Signout';
 import './style.css';
 
 class Hamburger extends React.Component {
@@ -11,10 +12,9 @@ class Hamburger extends React.Component {
     return (
         <div>
             <Menu pageWrapId={"page-wrap"} outerContainerId={ "outer-container" }>
-                {/* <a id="home" className="menu-item" href="/">Home</a>
-                <a id="about" className="menu-item" href="/about">About</a>
-                <a id="contact" className="menu-item" href="/contact">Contact</a>
-                <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
+                {
+                  <Signout/>
+                }
             </Menu>
         </div>
     );
