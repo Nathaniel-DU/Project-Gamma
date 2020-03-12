858195d5936ba57b08df7a2ccdb557eca599070f
Bradley Knutson <bradley@bknutson.com>
2020-03-12 10:23:38 -0600

    Modified route to allow entire domain to be served to index.html

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(`/user`, userRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
-  app.get(`/`, function(req, res) {
+  app.get(`*`, function(req, res) {
     res.sendFile(path.join(__dirname, `build`, `index.html`));
   });
 }
