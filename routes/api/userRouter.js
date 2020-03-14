6d88b464095fd0ce596d535b568403bceded34e7
Brittany Huber <brittanyleighhuber@gmail.com>
2020-03-13 20:12:27 -0600

    Added reverse geocoding to convert coordinates to address

--- routes/api/userRouter.js
+++ routes/api/userRouter.js
@@ -17,11 +17,38 @@ userRouter.route(`/friends`)
                         if(friendObj.onEvent){
                             db.Location.findById(friendObj.locations[friendObj.locations.length - 1])
                                 .then(locationList => {
-                                    friend.location = locationList.locations[locationList.locations.length - 1];
+                                    //const locationStr = that stuff
+                                    //axios call then pass in locationStr as the the lat long, friend.location = result of axios call 
+                                    //in the .then goes the the rest of this
+
+                                    const locationStr = locationList.locations[locationList.locations.length -1];
+                                    axios({
+                                        "method": "GET",
+                                        "url": "https://google-maps-geocoding.p.rapidapi.com/geocode/json",
+                                        "headers":{
+                                            "content-type":"application/octet-stream",
+                                            "x-rapidapi-host":"google-maps-geocoding.p.rapidapi.com",
+                                            "x-rapidapi-key": process.env.REVERSE_GEOCODING_API_KEY
+                                    }, "params": {
+                                        "language": "en",
+                                        "latlng": locationStr
+                                    }
+                                }).then((response) => {
+                                    console.log(response.data.results[0].formatted_address);
+                                    friend.location = response.data.results[0].formatted_address;
                                     finalFriendsList.push(friend);
                                     if(finalFriendsList.length === req.user.friendsList.length){
                                         res.json(finalFriendsList);
                                     }
+                                }).catch((err) => {
+                                    console.log(err)
+                                });
+
+                                    // friend.location = locationList.locations[locationList.locations.length - 1];
+                                    // finalFriendsList.push(friend);
+                                    // if(finalFriendsList.length === req.user.friendsList.length){
+                                    //     res.json(finalFriendsList);
+                                    // }
                                 })
                                 .catch(err => {
                                     console.log(err);
