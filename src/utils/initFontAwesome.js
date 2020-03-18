5edb7c73d2d242934bfb0c07e234ec4dcbe9e26b
Brittany Huber <brittanyleighhuber@gmail.com>
2020-03-17 22:02:04 -0600

    Added font awesome icons

--- src/utils/initFontAwesome.js
+++ src/utils/initFontAwesome.js
@@ -1,10 +1,18 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faLink, faPowerOff, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faLink, faPowerOff, faUser, faStopCircle, faMapMarkedAlt, faPhoneVolume, faCar, faExclamationCircle, faWalking, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 function initFontAwesome() {
-  library.add(faLink);
-  library.add(faUser);
-  library.add(faPowerOff);
+  // library.add(faLink);
+  // library.add(faUser);
+  // library.add(faPowerOff);
+  // library.add(faStopCircle);
+  // library.add(faMapMarkedAlt);
+  // library.add(faPhoneVolume);
+  // library.add(faCar);
+  // library.add(faExclamationCircle);
+  // library.add(faWalking);
+
+  library.add(faLink, faUser, faPowerOff, faStopCircle, faMapMarkedAlt, faPhoneVolume, faCar, faExclamationCircle, faWalking);
 }
 
 export default initFontAwesome;
