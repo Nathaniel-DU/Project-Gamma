6f816e4831d5de914f52a0206c426b0cbb5ecccf
Brittany Huber <brittanyleighhuber@gmail.com>
2020-03-20 16:53:58 -0600

    Added confirmation modal when deleting friend, friend form also clears after sending invite

--- src/components/DeleteFriend/index.js
+++ src/components/DeleteFriend/index.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserMinus } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import './style.css';
 import axios from 'axios';
 
 export default class DeleteFriend extends Component {
@@ -9,13 +11,41 @@ export default class DeleteFriend extends Component {
         axios.get(`/user/remove/${this.props.userId}`)
     }
 
-    render(){
-
-        return (
-            <button onClick={this.deleteUser}>
-                <FontAwesomeIcon className="fa-icon" icon={faUserMinus} />
-            </button>
-        )
+    state = {
+        modal1: false
     }
 
+    toggle = nr => () => {
+        let modalNumber = 'modal' + nr
+        this.setState({
+            [modalNumber]: !this.state[modalNumber]
+        });
+    }
+
+    toggleOff = nr => () => {
+        let modalNumber = 'modal' + nr;
+        this.setState({
+            [modalNumber]: !this.state[modalNumber]
+        });
+        this.deleteUser();
+    }
+
+    render() {
+
+        return (
+            <MDBContainer>
+                <MDBBtn size="sm" className="delete-btn" onClick={this.toggle(1)}><FontAwesomeIcon className="fa-icon delete-icon" icon={faTimes} /></MDBBtn>
+                <MDBModal isOpen={this.state.modal1} toggle={this.toggle(1)} centered>
+                    <MDBModalHeader className="modal-header" toggle={this.toggle(1)}>Confirm Deletion</MDBModalHeader>
+                    <MDBModalBody className="modal-body">
+                        Are you sure you want to remove this friend from your friend's list?
+                    </MDBModalBody>
+                    <MDBModalFooter>
+                        <MDBBtn className="close-mdl" onClick={this.toggle(1)}>Cancel</MDBBtn>
+                        <MDBBtn className="confirm-delete-btn" onClick={this.toggleOff(1)}>Confirm</MDBBtn>
+                    </MDBModalFooter>
+                </MDBModal>
+            </MDBContainer>
+        )
+    }
 }
