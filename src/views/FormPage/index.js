0255cbd34a3df97b27396537b8e3ad88c2cf9d82
Brittany Huber <brittanyleighhuber@gmail.com>
2020-03-21 12:12:24 -0600

    Removed divs to have consistent sizing on each form box

--- src/views/FormPage/index.js
+++ src/views/FormPage/index.js
@@ -7,12 +7,12 @@ import "./style.css"
 class FormPage extends Component {
   render() {
     return (
-      <Register/>
-      
+      <Register />
+
     );
   }
 }
-const validPhoneRegex=RegExp(/^([0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$/i)
+const validPhoneRegex = RegExp(/^([0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$/i)
 const validEmailRegex = RegExp(/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i);
 const validateForm = (errors) => {
   let valid = true;
@@ -36,7 +36,7 @@ class Register extends Component {
         firstName: '',
         lastName: '',
         email: '',
-        phoneNumber:'',
+        phoneNumber: '',
         password: '',
       }
     };
@@ -47,32 +47,32 @@ class Register extends Component {
     let errors = this.state.errors;
 
     switch (name) {
-      case 'firstName': 
-        errors.firstName = 
+      case 'firstName':
+        errors.firstName =
           value.length < 2
             ? 'First Name must be 2 characters long!'
             : '';
         break;
-        case 'lastName': 
-        errors.lastName = 
+      case 'lastName':
+        errors.lastName =
           value.length < 2
             ? 'Last Name must be 2 characters long!'
             : '';
         break;
-      case 'email': 
-        errors.email = 
+      case 'email':
+        errors.email =
           validEmailRegex.test(value)
             ? ''
             : 'Email is not valid!';
         break;
       case 'phoneNumber':
-        errors.phoneNumber=
-        validPhoneRegex.test(value)  
-        ? ''
-        : 'Must Be A Valid Phone Number';
+        errors.phoneNumber =
+          validPhoneRegex.test(value)
+            ? ''
+            : 'Must Be A Valid Phone Number';
         break;
-      case 'password': 
-        errors.password = 
+      case 'password':
+        errors.password =
           value.length < 8
             ? 'Password must be 8 characters long!'
             : '';
@@ -81,12 +81,12 @@ class Register extends Component {
         break;
     }
 
-    this.setState({errors, [name]: value});
+    this.setState({ errors, [name]: value });
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    if(validateForm(this.state.errors)) {
+    if (validateForm(this.state.errors)) {
       const user = {
         firstName: this.state.firstName,
         lastName: this.state.lastName,
@@ -96,17 +96,17 @@ class Register extends Component {
       }
       axios.post(`/auth/signup`, user)
 
-      .then(res => {
-        window.location = '/home';
-      });
+        .then(res => {
+          window.location = '/home';
+        });
       console.info('Valid Form')
-    }else{
+    } else {
       console.error('Invalid Form')
     }
   }
 
   render() {
-    const {errors} = this.state;
+    const { errors } = this.state;
     return (
       <div className='wrapper'>
         <div className='form-wrapper'>
@@ -115,46 +115,38 @@ class Register extends Component {
             <div className='firstName'>
               <label htmlFor="firstName">First Name</label>
               <input type='text' name='firstName' onChange={this.handleChange} />
-              {errors.firstName.length > 0 && 
+              {errors.firstName.length > 0 &&
                 <span className='error'>{errors.firstName}</span>}
-            </div>
-            <div className='lastName'>
               <label htmlFor="lastName">Last Name</label>
               <input type='text' name='lastName' onChange={this.handleChange} />
-              {errors.lastName.length > 0 && 
+              {errors.lastName.length > 0 &&
                 <span className='error'>{errors.lastName}</span>}
-            </div>
-            <div className='email'>
               <label htmlFor="email">Email</label>
               <input type='email' name='email' onChange={this.handleChange} />
-              {errors.email.length > 0 && 
+              {errors.email.length > 0 &&
                 <span className='error'>{errors.email}</span>}
-            </div>
-            <div className='phoneNumber'>
+                <br/>
               <label htmlFor="phoneNumber">Phone Number</label>
               <input type='phoneNumber' name='phoneNumber' onChange={this.handleChange} />
-              {errors.phoneNumber.length > 0 && 
+            
+              {errors.phoneNumber.length > 0 &&
                 <span className='error'>{errors.phoneNumber}</span>}
-            </div>
-            <div className='password'>
+                <br/>
               <label htmlFor="password">Password</label>
               <input type='password' name='password' onChange={this.handleChange} />
-              {errors.password.length > 0 && 
+              {errors.password.length > 0 &&
                 <span className='error'>{errors.password}</span>}
-            </div>
-            <div className='info'>
               <small>Password must be at least eight characters in length.</small>
-            </div>
-            
               <button className="sign-up-button">Sign Up</button>
-          
+            </div>
           </form>
         </div>
       </div>
+
     );
   }
 }
-          
-          
-                 
+
+
+
 export default FormPage;
