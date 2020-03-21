8077b2e2d1b909f002c2f99fdeaf25d26715b341
Bradley Knutson <bradley@bknutson.com>
2020-03-21 16:45:22 -0600

    Added response to prevent multiple friend invites

--- routes/api/friendRouter.js
+++ routes/api/friendRouter.js
@@ -3,40 +3,6 @@ const sgMail = require(`@sendgrid/mail`);
 const db = require(`../../models`);
 sgMail.setApiKey(process.env.SENDGRID_API);
 
-//Invite
-friendRouter.route(`/invite/:friendid`)
-    .get((req, res, next) => {
-        db.User.findByIdAndUpdate(req.params.userid, {
-            $addToSet: {
-                friendsPending: req.params.friendid
-            }
-        })
-        .then(invitee => {
-            if(invitee){
-                db.User.findByIdAndUpdate(req.params.friendid, {
-                    $addToSet: {
-                        friendsInvited: req.params.userid
-                    }
-                })
-                .then(invited => {
-                    if(invited){
-                        sgMail.send({
-                            to: invited.email,
-                            from: `info@bknutson.com`,
-                            subject: `${invitee.firstName} ${invitee.lastName} has invited you as a friend on StaySafe`,
-                            text: `Click this link to accept the friend request https://staysafeapp.herokuapp.com/user/${invited._id}/friends/accept/${invitee._id}`,
-                            html: `Click this link to accept the friend request https://staysafeapp.herokuapp.com/user/${invited._id}/friends/accept/${invitee._id}`
-                        });
-                    }
-                })
-                .catch(err => console.log(err))
-            }
-        })
-        .catch(err => console.log(err))
-    })
-
-
-
 
 //Accept
 friendRouter.route(`/accept/:friendid/confirmed`)
