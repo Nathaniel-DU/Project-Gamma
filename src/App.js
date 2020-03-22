f44fb14f8d54d7fec608e56c53d32527b122a140
Bradley Knutson <bradley@bknutson.com>
2020-03-21 23:12:40 -0600

    Added routes to confirm no access to login/create/root for authenticated users

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-import { Container } from "reactstrap";
 import FormPage from "./views/FormPage";
-import Loading from "./components/Loading";
 import Home from "./views/Home";
 import history from "./utils/history";
 import StartLocation from "./views/StartLocation";
-import UpdateView from "./views/UpdateView";
 import Login from "./views/LoginPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AcceptedFriendRequest from "./views/AcceptedFriendRequest";
 import EditProfile from "./views/EditProfile";
-import isAuthenticated from "./utils/isAuthenticated";
+import UnauthenticatedRoute from './components/UnauthenticatedRoute';
 
 // styles
 import "./App.css";
@@ -26,14 +23,10 @@ const App = () => {
     <Router history={history}>
       <div id="app" className="d-flex flex-column h-100">
         <Switch>
-          <Route exact path="/">
-            {isAuthenticated ? <Redirect to="/home" /> : <Home/>}
-          </Route>
-          <Route exact path="/auth/create">
-            {isAuthenticated ? <Redirect to="/home"/> : <FormPage/>}
-          </Route>
+          <UnauthenticatedRoute exact path="/" component={Home}/>
+          <UnauthenticatedRoute exact path="/auth/create" component={FormPage}/>
           <ProtectedRoute path="/home" component={StartLocation} />
-          <Route path="/auth/login" exact component={Login}/>
+          <UnauthenticatedRoute path="/auth/login" exact component={Login}/>
           <Route path="/user/:userid/friends/accept/:friendid" component={AcceptedFriendRequest}/>
           <ProtectedRoute path="/edit" exact component ={EditProfile} />
           <Route path="*"><Redirect to="/"/></Route>
--- src/utils/isAuthenticated.js
+++
@@ -1,9 +0,0 @@
-const axios = require(`axios`);
-
-function isAuthenticated() {
-    return new Promise((resolve, reject) => resolve(axios.get(`/auth/isauthenticated`).then(res => res.status === 200)))
-}
-
-export default isAuthenticated;
-
-
