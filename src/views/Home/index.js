13bbf6ccf2640cfaab9f7f428baa0cd9b359362a
Nicholas Herrick <nth.herrick@gmail.com>
2020-03-24 10:20:40 -0600

    fixed imported Button styling in Home page view, imported style.css instead

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from "react";
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
-import Button from '../../components/Button/';
-import Logo from '../../assets/logo.png'
+import '../../components/Button/style.css';
+import Logo from '../../assets/logo.png';
 import './style.css';
 
 const Home = () => {
